fdaac53478dde24f19164fb67b682a2521c8ad41
expruc <eygabi01@gmail.com>
2026-06-21 14:48:15 +0300

    change for v1.0.1

--- hello_world.py
+++ hello_world.py
@@ -2,5 +2,5 @@ def lambda_handler(event, context):
     print("Hello from CI/CD!")
     return {
         "statusCode": 200,
-        "body": "Hello from CI/CD!"
+        "body": "Hello from CI/CD! This is a change!"
     }
